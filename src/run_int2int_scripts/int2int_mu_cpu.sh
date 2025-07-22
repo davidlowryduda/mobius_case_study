8778e6f5c6528258f6abf6b24b9e200043f5d1bf
David Lowry-Duda <david@lowryduda.com>
2025-07-22 15:42:30 -0400

    Use actual numbers
    The previous numbers were for testing this make setup. Now actual
    numbers for true testing are given.

--- src/run_int2int_scripts/int2int_mu_cpu.sh
+++ src/run_int2int_scripts/int2int_mu_cpu.sh
@@ -20,4 +20,4 @@ function abspath {
 mkdir -p ../../models/
 
 
-python3 ../../Int2Int/train.py --num_workers 0 --dump_path "`abspath ../../models/`" --exp_name basic --exp_id 1 --train_data "`abspath ../../input/mu_modp_and_p.txt.train`" --eval_data "`abspath ../../input/mu_modp_and_p.txt.test`" --eval_size 10 --epoch_size 50 --operation data --data_types 'int[200]:range(-1,2)' --optimizer 'adam_inverse_sqrt,lr=0.00025' --max_epoch 201 --cpu True
+python3 ../../Int2Int/train.py --num_workers 0 --dump_path "`abspath ../../models/`" --exp_name basic --exp_id 1 --train_data "`abspath ../../input/mu_modp_and_p.txt.train`" --eval_data "`abspath ../../input/mu_modp_and_p.txt.test`" --eval_size 10000 --epoch_size 50000 --operation data --data_types 'int[200]:range(-1,2)' --optimizer 'adam_inverse_sqrt,lr=0.00025' --max_epoch 201 --cpu True
